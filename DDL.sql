9ea9ad19cd50b21cfd15348dde0bc82feb866f4b
eunseo-76 <eunseolee19@gmail.com>
2024-08-05 17:21:09 +0900

    Update : DDL.sql
    네이밍 규칙 준수, 가능한 FK 전부 삭제

--- DDL.sql
+++ DDL.sql
@@ -1,164 +1,74 @@
--- 1. 유저 권한 및 DB 생성
-
-show databases;
-
-use mysql;
-
-select * from user;
-
-create database toolmanager;
-
-show grants for 'test'@'%';
-
-grant all privileges on toolmanager.* to 'test'@'%';
-
-use toolmanager;
-
-
--- 2. 테이블 생성
-
-drop table if exists customer cascade;
-drop table if exists tool cascade;
-drop table if exists jig_type cascade;
-
-create table if not exists customer
+CREATE TABLE IF NOT EXISTS CUST_INFO_TB
 (
-	  customer_id	int auto_increment not null primary key comment '고객ID'
-	, customer_name	varchar(50) not null comment '고객명'
-	, customer_nickname	varchar(50) not null comment '고객닉네임'
-	, responsibility	varchar(50) not null comment '담당자'
-	, phone1	varchar(13) not null comment '연락처1'
-	, phone2	varchar(13) comment '연락처2'
-	, email	varchar(255) comment '이메일'
-	, website	varchar(255) comment '웹사이트'
-	, payment_day	varchar(255) comment '결제일'
-	, is_deleted	BOOLEAN COMMENT '삭제여부'
-	, deleted_date	DATETIME COMMENT '삭제일'
-	, delete_user	INT COMMENT '삭제한사용자'
-);
-alter table customer add constraint fk_delete_user foreign key (delete_user) references user(user_id);
-
-desc customer;
-
-CREATE table if not exists tool
-(
-	  tool_id	int auto_increment not null primary key comment '툴ID'
-	, j_serial	int comment '지그시리얼넘버'
-	, s_serial int comment '제판시리얼넘버'
-	, customer_id	int not null comment '고객ID'
-	, tool_number	varchar(255) not null comment '툴넘버'
-	, model_number	varchar(255) not null comment '모델넘버'
-	, j_type	int comment '지그종류'
-	, j_thickness int comment '지그두께'
-	, j_cutting	int comment '지그가공종류'
-	, j_hole_count	int comment '지그홀개수'
-	, s_type	int comment '제판종류'
-	, s_mesh	int comment '제판메쉬'
-	, s_emulsion	int comment '제판유제종류'
-	, arrival_date	date comment '입고일'
-	, memo	varchar(255) comment '메모'
-	, jig_or_ss	char(1) not null comment '지그제판구분'
-	, is_deleted	BOOLEAN NOT NULL DEFAULT 0 COMMENT '삭제여부'
-	, deleted_date DATETIME COMMENT '삭제일'
-	, delete_user	INT COMMENT '삭제한사용자'
-);
-
--- 테이블 순서 구분 없이 생성하기 위해 외래 키 제약 조건 별도 추가
-alter table tool add constraint fk_j_serial foreign key (j_serial) references jig_serial(jig_serial_id);
-alter table tool add constraint fk_s_serial foreign key (s_serial) references ss_serial(ss_serial_id);
-alter table tool add constraint fk_customer_id foreign key (customer_id) references customer(customer_id);
-alter table tool add constraint fk_jig_type foreign key (j_type) references jig_type(jig_type_id);
-alter table tool add constraint fk_jig_thickness foreign key (j_thickness) references jig_thickness(jig_thickness_id);
-alter table tool add constraint fk_jig_cutting foreign key (j_cutting) references jig_cutting(jig_cutting_id);
-alter table tool add constraint fk_ss_type foreign key (s_type) references ss_type(ss_type_id);
-alter table tool add constraint fk_ss_mesh foreign key (s_mesh) references ss_mesh(ss_mesh_id);
-alter table tool add constraint fk_ss_emulsion foreign key (s_emulsion) references ss_emulsion_type(ss_emulsion_id);
-alter table tool add constraint fk_delete_user_tool FOREIGN KEY (delete_user) REFERENCES user(user_id);
-
-alter table tool add constraint check_jig_or_ss check (
-    (jig_or_ss = 'j' and j_serial is not null and j_type is not null and j_thickness is not null and j_cutting is not null and j_hole_count is not null
-    and s_serial is null and s_type is null and s_mesh is null and s_emulsion is null)
-    or 
-    (jig_or_ss = 's' and j_serial is null and j_type is null and j_thickness is null and j_cutting is null and j_hole_count is null
-    and s_serial is not null and s_type is not null and s_mesh is not null and s_emulsion is not null)
+	  CUST_ID	INT AUTO_INCREMENT NOT NULL PRIMARY KEY COMMENT '고객ID'
+	, CUST_NM	VARCHAR(50) NOT NULL COMMENT '고객명'
+	, CUST_CD	VARCHAR(50) NOT NULL COMMENT '고객코드'
+	, PIC	VARCHAR(50) NOT NULL COMMENT '담당자'
+	, PHONE1	VARCHAR(13) NOT NULL COMMENT '연락처1'
+	, PHONE2	VARCHAR(13) NOT NULL COMMENT '연락처2'
+	, EMAIL_ADDR	VARCHAR(255) COMMENT '이메일'
+	, WEBSITE	VARCHAR(255) COMMENT '웹사이트'
+	, PAY_DT	VARCHAR(255) COMMENT '결제일'
+	, DEL_YN	BOOLEAN DEFAULT 0 COMMENT '삭제여부'
+	, DEL_DT	DATETIME NOT NULL COMMENT '삭제일'
+	, CUST_DEL_USR INT COMMENT '삭제한 사용자'
 );
 
 
-
-
-desc tool;
-
-CREATE table if not exists jig_type
+CREATE TABLE IF NOT EXISTS TOOL_INFO_TB
 (
-	  jig_type_id	int auto_increment not null primary key comment '지그종류'
-	, jig_type_name	varchar(50) not null comment '지그종류이름'
+	  TOOL_ID	INT AUTO_INCREMENT NOT NULL PRIMARY KEY COMMENT '툴ID'
+	, SERIAL_NO	INT NOT NULL UNIQUE COMMENT '시리얼넘버'
+	, CUST_ID	INT NOT NULL COMMENT '고객ID'
+	, TOOL_NO	VARCHAR(255) NOT NULL COMMENT '툴넘버'
+	, MODEL_NO	VARCHAR(255) NOT NULL COMMENT '모델넘버'
+	, J_TYPE	VARCHAR(50) COMMENT '모델넘버'
+	, J_THICKNESS	VARCHAR(50) COMMENT '지그두께'
+	, J_CUTTING	VARCHAR(50) COMMENT '지그가공종류'
+	, J_HOLE_CNT	INT COMMENT '지그홀개수'
+	, S_TYPE	VARCHAR(50) COMMENT '제판종류'
+	, S_MESH	VARCHAR(50) COMMENT '제판메쉬종류'
+	, S_EMULSION	VARCHAR(50) COMMENT '제판유제종류'
+	, ARRIVAL_DT	DATE COMMENT '입고일'
+	, MMEMO	VARCHAR(255) COMMENT '메모'
+	, JIG_OR_SS	CHAR(1) NOT NULL COMMENT ' 지그제판구분'
+	, DEL_YN	BOOLEAN	NOT NULL COMMENT '삭제여부'
+	, DEL_DT	DATETIME	COMMENT '삭제일'
+	, TOOL_DEL_USR	INT COMMENT '삭제한사용자'
 );
 
-desc jig_type;
-
-CREATE table if not exists jig_thickness
+CREATE TABLE IF NOT EXISTS USR_INFO_TB
 (
-	  jig_thickness_id	int auto_increment not null primary key comment '지그두께ID'
-	, jig_thickness_name	varchar(50) not null comment '지그두께이름'
+	  USR_ID INT AUTO_INCREMENT NOT NULL PRIMARY KEY COMMENT '툴ID'
+	, USR_EMAIL_ADDR	VARCHAR(255) NOT NULL COMMENT '이메일'
+	, USR_PW	VARCHAR(255) NOT NULL COMMENT '비밀번호'
+	, UST_NM	VARCHAR(50) NOT NULL COMMENT '실명'
+	, USR_NICK	VARCHAR(50) NOT NULL COMMENT '닉네임'
+	, BIRTH_DT	DATE NOT NULL COMMENT '생년월일'
+	, AUTH	CHAR(1) NOT NULL COMMENT '역할'
+	, DEL_YN	BOOLEAN NOT NULL COMMENT '탈퇴여부'
+	, DEL_DT DATETIME COMMENT '탈퇴일'
 );
 
-desc jig_thickness;
+ALTER TABLE cust_info_tb
+ADD CONSTRAINT FK_CUST_DEL_USER
+FOREIGN KEY(CUST_DEL_USR) REFERENCES USR_INFO_TB(USR_ID);
 
-CREATE table if not exists jig_cutting
-(
-	  jig_cutting_id int auto_increment not null primary key comment '지그가공ID'
-	, jig_cutting_name	varchar(50) not null comment '지그가공이름'
-);
+ALTER TABLE tool_info_tb
+ADD CONSTRAINT FK_TOOL_DEL_USR
+FOREIGN KEY(TOOL_DEL_USR) REFERENCES USR_INFO_TB(USR_ID);
 
-desc jig_cutting;
+ALTER TABLE tool_info_tb
+ADD CONSTRAINT FK_CUST_ID
+FOREIGN KEY(CUST_ID) REFERENCES CUST_INFO_TB(CUST_ID);
 
-CREATE table if not exists ss_type
-(
-	  ss_type_id int auto_increment not null primary key comment '제판종류'
-	, ss_type_name	varchar(50) not null comment '제판종류이름'
-);
 
-desc ss_type;
-
-CREATE table if not exists ss_mesh
-(
-	  ss_mesh_id	int auto_increment not null primary key comment '제판메쉬ID'
-	, ss_mesh_count	int not null comment '제판메쉬개수'
-);
-
-desc ss_mesh;
-
-CREATE table if not exists ss_emulsion_type
-(
-	  ss_emulsion_id int auto_increment not null primary key comment '제판유제ID'
-	, ss_emulsion_name	varchar(50) not null comment '제판유제이름'
-);
-
-desc ss_emulsion_type;
-
-CREATE table if not exists jig_serial
-(
-	  jig_serial_id int not null primary key comment '지그시리얼ID'
-);
-
-desc jig_serial;
-
-CREATE table if not exists ss_serial
-(
-	  ss_serial_id int not null primary key comment '제판시리얼ID'
-);
-
-desc ss_serial;
-
-CREATE TABLE if NOT EXISTS user
-(
-	  user_id	INT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '사용자ID'
-	, email	VARCHAR(255) NOT NULL COMMENT '이메일'
-	, PASSWORD	VARCHAR(255) NOT NULL COMMENT '비밀번호'
-	, NAME	VARCHAR(255) NOT NULL COMMENT '실명'
-	, nickname	VARCHAR(50) NOT NULL COMMENT '닉네임'
-	, birth	DATE NOT NULL COMMENT '생년월일'
-	, auth	CHAR(1) NOT NULL COMMENT '역할'	-- enum? boolean? char?=
-	, is_active	BOOLEAN NOT NULL COMMENT '탈퇴여부'
-	, deactivated_date	DATETIME COMMENT '탈퇴일'
+ALTER TABLE TOOL_INFO_TB 
+ADD CONSTRAINT JIG_OR_SS 
+CHECK (
+	(JIG_OR_SS = 'J' AND J_TYPE IS NOT NULL AND J_THICKNESS IS NOT NULL AND J_CUTTING IS NOT NULL AND J_HOLE_CNT IS NOT NULL
+	AND S_TYPE IS NULL AND S_MESH IS NULL AND S_EMULSION IS NULL)
+	OR
+	(JIG_OR_SS = 'S' AND J_TYPE IS NULL AND J_THICKNESS IS NULL AND J_CUTTING IS NULL AND J_HOLE_CNT IS NULL
+	AND S_TYPE IS NOT NULL AND S_MESH IS NOT NULL AND S_EMULSION IS NOT NULL)
 );
